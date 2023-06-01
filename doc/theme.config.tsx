fffc9fdd362c3684d2505c14b54ea0e66b3022e3
ali <aliahadmd@outlook.com>
2023-06-02 01:14:53 +0800

    docs added

---
+++ doc/theme.config.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { DocsThemeConfig } from 'nextra-theme-docs'
+
+const config: DocsThemeConfig = {
+  logo: <span>My Project</span>,
+  project: {
+    link: 'https://github.com/aliahadmd/cypherscript',
+  },
+  chat: {
+    link: 'https://discord.com',
+  },
+  docsRepositoryBase: 'https://github.com/aliahadmd/cypherscript',
+  footer: {
+    text: 'cypherscript',
+  },
+}
+
+export default config
